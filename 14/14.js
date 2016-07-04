5b04f1da2d1f987dc4365a57e442d0dd55c27758
Ivoirians <kvns801@gmail.com>
2016-07-04 02:55:20 -0400

    Typo

--- 14/14.js
+++ 14/14.js
@@ -3,25 +3,24 @@ var app = angular.module('counterApp', []);
 app.controller('CounterController', function($scope, $http) {
     
 	document.getElementById("counter_button").onclick = function() {
-			$http({
-				method: 'GET',
-				url: '/svc/counter/get'
-			}).then(function successCallback(response) {
-				$scope.counter = response;
-			}, function errorCallback(response) {
-				console.log("Service not found.")
-			});
-		};
+		$http({
+			method: 'GET',
+			url: '/svc/counter/get'
+		}).then(function successCallback(response) {
+			$scope.counter = response;
+		}, function errorCallback(response) {
+			console.log("Service not found.")
+		});
+	};
 
     $scope.addCounter = function() {
-			$http({
-				method: 'GET',
-				url: '/svc/counter/add'
-			}).then(function successCallback(response) {
-				$scope.counter = response;
-			}, function errorCallback(response) {
-				console.log("Service not found.")
-			});
-		};
-    }
+		$http({
+			method: 'GET',
+			url: '/svc/counter/add'
+		}).then(function successCallback(response) {
+			$scope.counter = response;
+		}, function errorCallback(response) {
+			console.log("Service not found.")
+		});
+	};
 });
