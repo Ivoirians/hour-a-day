00f80a736b3a2eaf13635430f63332ed8808aac7
Kevin Shen <kevin.shen@admins-MacBook-Pro-5.local>
2016-07-07 23:26:21 -0700

    A tiny Day 18

---
+++ 18/18.js
@@ -0,0 +1,35 @@
+var app = angular.module('commentsApp', []);
+var pageNumber = window.location.pathname
+app.controller('CommentController', function($scope, $http) {
+
+    $scope.comments = [];
+	$http({
+		method: 'GET',
+		url: '/svc/comments/get/' + pageNumber
+	}).then(function successCallback(response) {
+		$scope.comments = response.data;
+	}, function errorCallback(response) {
+		console.log("Service not found.")
+	});
+
+    $scope.addComment = function() {
+    	if ($scope.comment)
+    	{
+			$http({
+				method: 'POST',
+				url: '/svc/comments/add',
+				data: {
+					username: $scope.username,
+					page_number: pageNumber,
+					comment_text: $scope.comment
+				}
+
+			}).then(function successCallback(response) {
+				$scope.comment = "";
+				console.log(response.data);
+			}, function errorCallback(response) {
+				console.log("Service not found.")
+			});
+		}
+	};
+});
